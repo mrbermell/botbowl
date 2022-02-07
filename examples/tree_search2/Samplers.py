2e844aee8810ff67c7a880703333ce9c33037870
mrbermell <mattias.br@gmail.com>
2022-02-07 22:36:51 +0100

    mock policy

--- examples/tree_search2/Samplers.py
+++ examples/tree_search2/Samplers.py
@@ -6,7 +6,8 @@ import numpy as np
 from more_itertools import collapse
 
 import botbowl
-from botbowl import ActionType, Action
+from botbowl import ActionType, Action, EnvConf, Setup
+from tests.util import get_game_turn
 
 NodeAction = botbowl.Action
 
@@ -103,3 +104,86 @@ class ActionSampler:
         return len(self.actions)
 
 
+class MockPolicy:
+    def __init__(self):
+        self.env_conf = EnvConf()
+        self.positional_types = set(self.env_conf.positional_action_types)
+        self.simple_types = set(self.env_conf.simple_action_types)
+
+        self.convert_function = {ActionType.MOVE: self.move_actions,
+                                 ActionType.START_HANDOFF: self.start_handoff_actions,
+                                 #ActionType.START_BLITZ: self.move_actions,
+                                 #ActionType.START_BLITZ: self.move_actions,
+                                 #ActionType.START_BLITZ: self.move_actions,
+        }
+
+        self.end_setup = False
+
+        self.player_actiontypes_without_move_actions = {botbowl.core.table.PlayerActionType.HANDOFF,
+                                                        botbowl.core.table.PlayerActionType.BLITZ}
+
+    def start_handoff_actions(self, game: botbowl.Game, action_choice):
+        ball = game.get_ball()
+        if ball is not None and ball.is_carried:
+            if ball.position in action_choice.positions:
+                return [Action(ActionType.START_HANDOFF, position=[ball.position])]
+        return []
+
+    def move_actions(self, game: botbowl.Game, action_choice) -> Iterable[Action]:
+        if game.get_player_action_type() in self.player_actiontypes_without_move_actions:
+            return []
+
+        if game.get_active_player().state.moves > 0:
+            return []
+
+        return [Action(ActionType.MOVE, position=pos) for pos in action_choice.positions]
+
+    def __call__(self, game: botbowl.Game) -> Tuple[float, np.ndarray, List[botbowl.Action]]:
+        actions: List[botbowl.Action] = []
+
+        if self.end_setup:
+            self.end_setup = False
+            return 0.0, np.array([1.0]), [Action(ActionType.END_SETUP)]
+
+        for action_choice in game.get_available_actions():
+            action_type = action_choice.action_type
+
+            if action_type in self.convert_function:
+                actions.extend(self.convert_function[action_type](game, action_choice))
+
+            elif action_type in self.positional_types:
+                if len(action_choice.positions)>0:
+                    positions = action_choice.positions
+                else:
+                    positions = [p.position for p in action_choice.players]
+
+                for pos in positions:
+                    actions.append(Action(action_type, position=pos))
+
+            elif action_choice.action_type in self.simple_types:
+                actions.append(Action(action_type))
+
+            elif type(game.get_procedure()) is Setup and action_type not in {ActionType.END_SETUP, ActionType.PLACE_PLAYER}:
+                actions.append(Action(action_type))
+                self.end_setup = True
+
+        probabilities = np.ones(len(actions))/len(actions)
+        #assert abs(np.sum(probabilities) - 1.0) < 0.00001
+
+        return 0.0, probabilities, actions
+
+def main():
+    policy = MockPolicy()
+
+    game = get_game_turn()
+    game.config.pathfinding_enabled = True
+    game.set_available_actions()
+
+    while not game.state.game_over:
+        _, _, actions = policy(game)
+        action = np.random.choice(actions, 1)[0]
+        game.step(action)
+        print(action)
+
+if __name__ == "__main__":
+    main()
