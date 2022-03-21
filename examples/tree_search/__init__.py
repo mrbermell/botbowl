3cc9ba35b2efdfacece926dec882d2ebad24c1de
mrbermell <mattias.br@gmail.com>
2022-03-21 19:11:53 +0100

    refactor structure of tree search module

--- examples/tree_search/__init__.py
+++ examples/tree_search/__init__.py
@@ -1,3 +1,4 @@
 from .SearchTree import SearchTree, ActionNode, ChanceNode, expand_action, Node
-from .Searchers import deterministic_tree_search_rollout, get_node_value, HeuristicVector, MCTS_Info
 from .Samplers import MockPolicy
+from .searchers import HeuristicVector, deterministic_tree_search_rollout, get_node_value
+from .searchers.deterministic import MCTS_Info
